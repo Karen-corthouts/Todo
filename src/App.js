4df7bd4cb194e4c816e4f2c8d4ddba07ff24235e
Karen Corthouts <karen.corthouts@intracto.com>
2022-03-15 00:58:17 +0100

    check and uncheck

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import {useEffect, useState} from 'react';
+import TodoItem from './components/TodoItem';
+import PageWrapper from "./components/PageWrapper";
+import "./styles/main.css";
+
+
+
 function App() {
-  return <div>hello world</div>;
+  const [todos, setTodos] = useState([]);
+
+  const getData = async () => {
+    const result = await fetch('http://localhost:3030/todos');
+    return await result.json();
+  };
+
+  useEffect(() => {
+    getData().then((res) => setTodos(res));
+  }, []);
+
+  return (
+    <PageWrapper>
+      <div>
+        <ul>
+          {todos.map((todo) => (
+            <TodoItem key={todo.id} {...todo} />
+          ))}
+        </ul>
+      </div>
+    </PageWrapper>
+  )
 }
 
 export default App;
